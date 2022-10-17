8dbd86fe45919c8d0790df6b7b98681e71eee802
ThaoCV <che.van.thao@jam-tech.jp>
2022-10-17 16:23:20 +0900

    connect to mongoDB

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require("morgan")
+const mongoose = require("mongoose");
 const app = express()
 const port = 3000
 
@@ -13,35 +14,62 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
-console.log("hello")
-// const { MongoClient } = require("mongodb");
-// // Replace the uri string with your MongoDB deployment's connection string.
-// const uri =
-//   "mongodb+srv://<koiirosora>:<17041996hb>@<cluster-url>?retryWrites=true&writeConcern=majority";
-// const client = new MongoClient(uri);
-// async function run() {
-//   try {
-//     await client.connect();
-//     // database and collection code goes here
-//     // insert code goes here
-//     // display the results of your operation
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close();
-//   }
-// }
-// run().catch(console.dir);
+// // Connect to DB
+// const mongoURI = "mongodb+srv://koiirosora:0123456789@cluster0.imywx6q.mongodb.net/test";
 
-// const db = client.db("sample_guides");
-// const coll = db.collection("comets");
+// mongoose.connect(mongoURI, {
+// useNewUrlParser: true,
+// useUnifiedTopology: true,
+// });
 
-// // insert code goes here
-// const docs = [
-//     {name: "Halley's Comet", officialName: "1P/Halley", orbitalPeriod: 75, radius: 3.4175, mass: 2.2e14},
-//     {name: "Wild2", officialName: "81P/Wild", orbitalPeriod: 6.41, radius: 1.5534, mass: 2.3e13},
-//     {name: "Comet Hyakutake", officialName: "C/1996 B2", orbitalPeriod: 17000, radius: 0.77671, mass: 8.8e12}
-//   ];
-//   const result = await coll.insertMany(docs);
+const { MongoClient } = require("mongodb");
+// Replace the uri string with your MongoDB deployment's connection string.
+const uri =
+"mongodb+srv://koiirosora:0123456789@cluster0.imywx6q.mongodb.net/test";
+const client = new MongoClient(uri);
+async function run() {
+  try {
+    await client.connect();
+    // database and collection code goes here
+    const db = client.db("sample_guides");
+    const coll = db.collection("comets");
 
-// // display the results of your operation
-// console.log(result.insertedIds);
+// insert code goes here
+    // const docs = [
+    //   {name: "Halley's Comet", officialName: "1P/Halley", orbitalPeriod: 75, radius: 3.4175, mass: 2.2e14},
+    //   {name: "Wild2", officialName: "81P/Wild", orbitalPeriod: 6.41, radius: 1.5534, mass: 2.3e13},
+    //   {name: "Comet Hyakutake", officialName: "C/1996 B2", orbitalPeriod: 17000, radius: 0.77671, mass: 8.8e12}
+    // ];
+    // insert code goes here
+    // const result = await coll.insertMany(docs);
+
+// update code goes here
+    const filter = { };
+    const updateDoc = {
+      $mul: {
+          radius: 1.60934
+      }
+    };
+    // update code goes here
+    const result = await coll.updateMany(filter, updateDoc);
+// delete code goes here 
+    
+    // const doc = {
+    //   orbitalPeriod: {
+    //     $gt: 5,
+    //     $lt: 85
+    //   }
+    // };
+    // delete code goes here
+    // const result = await coll.deleteMany(doc);
+
+    // display the results of your operation
+    console.log(result.insertedIds);
+    console.log("Number of documents updated: " + result.modifiedCount);
+    console.log("Number of documents deleted: " + result.deletedCount);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
